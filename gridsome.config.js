9097729a4e23490ade97a26636448eff002e5af9
Hunter Nunnery <hunter.nunnery@yahoo.com>
2019-11-05 19:14:08 -0500

    Removed Netlify CMS npm package

--- gridsome.config.js
+++ gridsome.config.js
@@ -53,12 +53,6 @@ module.exports = {
           ]
         }
       }
-    },
-    {
-      use: `gridsome-plugin-netlify-cms`,
-      options: {
-        publicPath: `/admin`
-      }
     }
   ]
 };
